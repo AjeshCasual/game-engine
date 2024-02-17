2aedbb1e955cc89e9e08ccc645fdf8e76e031b8d
Ajesh Casual <ajeshsharma3@gmail.com>
2024-02-18 01:04:17 +0530

    .gitignore is now working

--- compile.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-g++ -o main.exe src/*.cpp -std=c99 -Wall -Iexternal -DPLATFORM_DESKTOP -lmsvcrt -lraylib -lopengl32 -lgdi32 -lwinmm -lkernel32 -lshell32 -luser32 -lbox2d -mwindows -I include -L lib 2> logs/g.txt
-main.exe > logs/main.txt
